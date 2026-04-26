b5fc9235381d919f4fc92a1db4b227c2a1eef063
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2026-04-26 19:18:21 +0100

    Update some core programs.
    Update SSSOM-Java to version 1.11.2 and RelationGraph to version 2.3.4.

--- src/incatools/odk/setup.py
+++ src/incatools/odk/setup.py
@@ -21,11 +21,11 @@ from jinja2 import Template
 
 ROBOT_SOURCE = "https://github.com/ontodev/robot/releases/download/v1.9.10/robot.jar"
 DICER_SOURCE = "https://github.com/gouttegd/dicer/releases/download/dicer-0.2.1/dicer-cli-0.2.1.jar"
-SSSOM_SOURCE = "https://github.com/gouttegd/sssom-java/releases/download/sssom-java-1.10.0/sssom-cli-1.10.0.jar"
+SSSOM_SOURCE = "https://github.com/gouttegd/sssom-java/releases/download/sssom-java-1.11.2/sssom-cli-1.11.2.jar"
 DOSDP_SOURCE = "https://github.com/INCATools/dosdp-tools/releases/download/v0.19.3/dosdp-tools-0.19.3.tgz"
-RELGR_SOURCE = "https://github.com/INCATools/relation-graph/releases/download/v2.3.3/relation-graph-cli-2.3.3.tgz"
+RELGR_SOURCE = "https://github.com/INCATools/relation-graph/releases/download/v2.3.4/relation-graph-cli-2.3.4.tgz"
 ODK_PLUGIN_SOURCE = "https://github.com/INCATools/odk-robot-plugin/releases/download/odk-robot-plugin-0.3.1/odk.jar"
-SSSOM_PLUGIN_SOURCE = "https://github.com/gouttegd/sssom-java/releases/download/sssom-java-1.10.0/sssom-robot-plugin-1.10.0.jar"
+SSSOM_PLUGIN_SOURCE = "https://github.com/gouttegd/sssom-java/releases/download/sssom-java-1.11.2/sssom-robot-plugin-1.11.2.jar"
 OBO_EPM_SOURCE = "https://raw.githubusercontent.com/biopragmatics/bioregistry/main/exports/contexts/obo.epm.json"
 
 ACTIVATION_TEMPLATE = Path(__file__).parent.resolve() / "activate.jinja2"
